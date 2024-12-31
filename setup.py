f52dd1c891b3a9cd4ef0d66cae77b28c41f1fe4d
koh43 <sktlgt93@gmail.com>
2024-12-31 15:12:05 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'pi_ros2_cv'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.xml')),
+    ],
+    install_requires=[
+        'setuptools'
+    ],
+    zip_safe=True,
+    maintainer='koh',
+    maintainer_email='sktlgt93@gmail.com',
+    description='ROS2 Package for Raspberry Pi Camera Modules using OpenCV',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+)
